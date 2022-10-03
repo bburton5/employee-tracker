c4e1c1f6d03cfd888f4877ea6c46f543f7fc575a
bburton5 <bburton5@uw.edu>
2022-10-03 15:59:11 -0700

    Added main inquirer options and switch cases

--- server.js
+++ server.js
@@ -14,3 +14,54 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the company_db database.`)
 );
+
+const prompts = async () => {
+  return inquirer
+    .prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+        ],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      console.log(answers.action);
+      switch (answers.action) {
+        case "View All Departments":
+          console.log("View All Departments Case");
+          break;
+        case "View All Roles":
+          console.log("View All Roles Case");
+          break;
+        case "View All Employees":
+          console.log("View All Employees Case");
+          break;
+        case "Add a Department":
+          console.log("Add a Department Case");
+          break;
+        case "Add a Role":
+          console.log("Add a Role Case");
+          break;
+        case "Add an Employee":
+          console.log("Add an Employee Case");
+          break;
+        case "Update an Employee Role":
+          console.log("Update an Employee Role");
+          break;
+        default:
+          console.log("Default Case");
+      }
+    });
+};
+
+prompts();
