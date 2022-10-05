cc6bea9a622435dbc7c69f12a2fd3ca7c7281de7
bburton5 <bburton5@uw.edu>
2022-10-04 22:04:54 -0700

    created individual functions for each case

--- server.js
+++ server.js
@@ -12,8 +12,166 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the company_db database.`)
 );
+// Function to query all departments
+let viewAllDepartments = () => {
+  db.query(`SELECT * FROM department;`, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table(result);
+    }
+  });
+};
 
-const employeeChoices = async () => {
+// Function to query all roles
+let viewAllRoles = () => {
+  db.query(`SELECT * FROM roles;`, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table(result);
+    }
+  });
+};
+
+// Function to query all employees
+let viewAllEmployees = () => {
+  db.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.department_name FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id ORDER BY employee.id; `,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.table(result);
+      }
+    }
+  );
+};
+
+// Function to add a department to db
+let addADepartment = () => {
+  inquirer
+    .prompt([
+      {
+        name: "departmentName",
+        type: "input",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then((answers) => {
+      console.log("inside department prompt");
+      db.query(
+        `INSERT INTO department (department_name)
+              VALUES (?)`,
+        answers.departmentName,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            db.query(`SELECT * FROM department`, (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.table(result);
+              }
+            });
+          }
+        }
+      );
+    });
+};
+
+// Function to add a role to db
+let addARole = () => {
+  inquirer
+    .prompt([
+      {
+        name: "roleTitle",
+        type: "input",
+        message: "What is the title of the role?",
+      },
+      {
+        name: "roleSalary",
+        type: "number",
+        message: "What is this role's salary?",
+      },
+      {
+        name: "roleDepartmentId",
+        type: "number",
+        message: "What department number is this role under?",
+      },
+    ])
+    .then((answers) => {
+      console.log("inside role prompt");
+      db.query(
+        `INSERT INTO roles (title, salary, department_id)
+              VALUES (?, ?, ?)`,
+        [answers.roleTitle, answers.roleSalary, answers.roleDepartmentId],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            db.query(`SELECT * FROM roles`, (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.table(result);
+              }
+            });
+          }
+        }
+      );
+    });
+};
+
+// Function to add an employee to db
+let addAnEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "employeeFirstName",
+        type: "input",
+        message: "What is the employee's first name?",
+      },
+      {
+        name: "employeeLastName",
+        type: "input",
+        message: "What is the employee's last name?",
+      },
+      {
+        name: "employeeRole",
+        type: "number",
+        message: "What is the employee's role?",
+      },
+    ])
+    .then((answers) => {
+      console.log("inside add employee prompt");
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id)
+              VALUES (?, ?, ?)`,
+        [
+          answers.employeeFirstName,
+          answers.employeeLastName,
+          answers.employeeRole,
+        ],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.table(result);
+              }
+            });
+          }
+        }
+      );
+    });
+};
+
+let employeeChoices = async () => {
   const employeeQuery = `SELECT CONCAT(employee.first_name, " ", employee.last_name) AS name FROM employee;`;
   const employees = await db.query(employeeQuery);
   return employees[0];
@@ -38,165 +196,30 @@ const prompts = async () => {
       },
     ])
     .then((answers) => {
-      console.log(answers);
-      console.log(answers.action);
       switch (answers.action) {
         case "View All Departments":
           console.log("Running View All Departments Case");
-          db.query(`SELECT * FROM department;`, (err, result) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.table(result);
-            }
-          });
+          viewAllDepartments();
           break;
         case "View All Roles":
           console.log("Running View All Roles Case");
-          db.query(`SELECT * FROM roles;`, (err, result) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.table(result);
-            }
-          });
+          viewAllRoles();
           break;
         case "View All Employees":
           console.log("Running View All Employees Case");
-          db.query(
-            `SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.department_name FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id ORDER BY employee.id; `,
-            (err, result) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.table(result);
-              }
-            }
-          );
+          viewAllEmployees();
           break;
         case "Add a Department":
           console.log("Running Add a Department Case");
-          inquirer
-            .prompt([
-              {
-                name: "departmentName",
-                type: "input",
-                message: "What is the name of the department?",
-              },
-            ])
-            .then((answers) => {
-              console.log("inside department prompt");
-              db.query(
-                `INSERT INTO department (department_name)
-              VALUES (?)`,
-                answers.departmentName,
-                (err, result) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    db.query(`SELECT * FROM department`, (err, result) => {
-                      if (err) {
-                        console.log(err);
-                      } else {
-                        console.table(result);
-                      }
-                    });
-                  }
-                }
-              );
-            });
+          addADepartment();
           break;
         case "Add a Role":
           console.log("Running Add a Role Case");
-          inquirer
-            .prompt([
-              {
-                name: "roleTitle",
-                type: "input",
-                message: "What is the title of the role?",
-              },
-              {
-                name: "roleSalary",
-                type: "number",
-                message: "What is this role's salary?",
-              },
-              {
-                name: "roleDepartmentId",
-                type: "number",
-                message: "What department number is this role under?",
-              },
-            ])
-            .then((answers) => {
-              console.log("inside role prompt");
-              db.query(
-                `INSERT INTO roles (title, salary, department_id)
-              VALUES (?, ?, ?)`,
-                [
-                  answers.roleTitle,
-                  answers.roleSalary,
-                  answers.roleDepartmentId,
-                ],
-                (err, result) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    db.query(`SELECT * FROM roles`, (err, result) => {
-                      if (err) {
-                        console.log(err);
-                      } else {
-                        console.table(result);
-                      }
-                    });
-                  }
-                }
-              );
-            });
+          addARole();
           break;
         case "Add an Employee":
           console.log("Running Add an Employee Case");
-          inquirer
-            .prompt([
-              {
-                name: "employeeFirstName",
-                type: "input",
-                message: "What is the employee's first name?",
-              },
-              {
-                name: "employeeLastName",
-                type: "input",
-                message: "What is the employee's last name?",
-              },
-              {
-                name: "employeeRole",
-                type: "number",
-                message: "What is the employee's role?",
-              },
-            ])
-            .then((answers) => {
-              console.log("inside add employee prompt");
-              db.query(
-                `INSERT INTO employee (first_name, last_name, role_id)
-              VALUES (?, ?, ?)`,
-                [
-                  answers.employeeFirstName,
-                  answers.employeeLastName,
-                  answers.employeeRole,
-                ],
-                (err, result) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    db.query(`SELECT * FROM employee`, (err, result) => {
-                      if (err) {
-                        console.log(err);
-                      } else {
-                        console.table(result);
-                      }
-                    });
-                  }
-                }
-              );
-            });
+          addAnEmployee();
           break;
         case "Update an Employee Role":
           console.log("Running Update an Employee Role Case");
